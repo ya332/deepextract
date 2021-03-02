7a4392b80ca1bcc8393d775b1ba95bc48600f5f6
ya332 <alparslanyigitcan@gmail.com>
2021-03-02 04:31:20 -0500

    Bump to v1.0.5

--- deepextract/__init__.py
+++ deepextract/__init__.py
@@ -1,5 +1,5 @@
 """deepextract - A Python library to parse JSON, YAML files and extract deeply nested keys."""
 
-__version__ = '1.0.4'
+__version__ = '1.0.5'
 __author__ = 'Yigit Alparslan <alparslanyigitcan@gmail.com>'
 __all__ = []
